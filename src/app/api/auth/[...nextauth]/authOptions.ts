cbe3482d45c67e7797c1ec66889536e8c0027e25
SheenOFTime <jakub.vaculik.2006@gmail.com>
2024-11-29 09:18:31 +0100

    Prisma addiont + running

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -2,10 +2,12 @@
 
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "./prisma"
 
 export const authOptions: NextAuthOptions = {
-
-    providers: [
+  adapter: PrismaAdapter(prisma),
+  providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
